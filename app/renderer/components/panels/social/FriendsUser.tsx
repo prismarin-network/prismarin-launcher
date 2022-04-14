d3a63b363893ae0b1df0cf69eda4eba562fde982
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-04-14 08:00:31 +0200

    Updated Design of Context Menu

--- app/renderer/components/panels/social/FriendsUser.tsx
+++ app/renderer/components/panels/social/FriendsUser.tsx
@@ -28,29 +28,16 @@ const FriendsUser = ({username, activity }: FriendsUserProps) => {
                 </div>
             </ContextMenuTrigger>
             <ContextMenu id={`contextMenu-${username}`}>
-                <MenuItem>
-                    <div className="flex items-center pr-3 space-x-3 group-hover:bg-black cursor-pointer min-w-[225px]">
-                        <UserAvatar activityStatus={activity.activityStatus} size="45px"/>
-                        <div className="leading-none">
-                            <h3 className="text-lg text-gray-300">
-                                {username}
-                            </h3>
-                            <h4 className="text-sm font-semibold text-gray-500">
-                                {activity.tagLine}
-                            </h4>
-                        </div>
-                    </div>
+                <MenuItem disabled={true}>
+                    View profile
                 </MenuItem>
-                <MenuItem divider={true} />
                 <MenuItem disabled={true}>
                     Invite to lobby
                 </MenuItem>
                 <MenuItem disabled={true}>
                     Send message
                 </MenuItem>
-                <MenuItem disabled={true}>
-                    View profile
-                </MenuItem>
+                <MenuItem divider={true} />
                 <MenuItem>
                     Remove friend
                 </MenuItem>
